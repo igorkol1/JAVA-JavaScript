ac83e2725feb3fe968cfa141276c633f59f5c05f
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-08-03 10:12:43 +0200

    Rozwiązanie zadań z dzień 4 sekcja a I

---
+++ 1_Zadania/Dzien_4/1_Wyszukiwanie_elementow/js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    //EX 1
+
+    // var links  = $('section.main li');
+    // links.addClass('borderClass');
+
+    //Ex 2
+
+    // var showMoreElem = $('.showMore');
+    // showMoreElem.eq(0).css('color', 'red');
+    //// elem.style.color = 'red'; -> pure js
+
+    //Ex 3
+
+
+
+});
